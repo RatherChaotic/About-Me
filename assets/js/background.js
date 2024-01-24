aeec95e36961a3932ce67dc7609847cae3d11fa2
RatherChaotic <OccationallyChaotic@outlook.com>
2024-01-24 08:57:45 -0500

    Changing Letters

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,43 +1,36 @@
 const canvas = document.getElementById('background');
 const context = canvas.getContext('2d');
 
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
-const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
-const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const nums = '0123456789';
-
-const alphabet = katakana + latin + nums;
+var AllChars = [];
+var alphabet = String
+for (var i=32; i<127; i++)
+    AllChars.push(String.fromCharCode(i));
+    alphabet += AllChars[i]
 
 const fontSize = 16;
-const columns = canvas.width / fontSize;
+const columns = canvas.width/fontSize;
 
 const rainDrops = [];
 
-for (let x = 0; x < columns; x++) {
+for( let x = 0; x < columns; x++ ) {
     rainDrops[x] = 1;
 }
-
-let retrieved_entity_ip = String;
-$.getJSON('https://api.ipify.org?format=jsonp&callback=?', function(data) {
-    console.log(data.ip.toString());
-
-    retrieved_entity_ip = data.ip.toString();
-});
 
 const draw = () => {
     context.fillStyle = 'rgba(0, 0, 0, 0.05)';
     context.fillRect(0, 0, canvas.width, canvas.height);
+
     context.fillStyle = '#0F0';
     context.font = fontSize + 'px monospace';
 
-    for (let i = 0; i < rainDrops.length; i++) {
-        const text = retrieved_entity_ip
-        context.fillText(text, (i * text.length), rainDrops[i] * fontSize);
+    for(let i = 0; i < rainDrops.length; i++)
+    {
+        const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+        context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
 
-        if (rainDrops[i] * fontSize > canvas.height && Math.random() > 0.975) {
+        if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.975){
             rainDrops[i] = 0;
         }
         rainDrops[i]++;
