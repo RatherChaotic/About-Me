bc8e5695628afe6741794bda96d09e4151081025
RatherChaotic <OccationallyChaotic@outlook.com>
2024-01-30 09:38:58 -0500

    Added Terminal

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,21 @@ let text = "An ambitious high-school student wanting to code all the things!";
 let speed = 75;
 const timer = ms => new Promise(res => setTimeout(res, ms));
 
+
+$(function() {
+    $('#terminal').terminal({
+        help: function() {
+            this.echo('help : prints helpful commands\nprint : print whatever text you like\ngoto : go to a certain .html page')
+        },
+        print: function(what) {
+            this.echo(what)
+        },
+        goto : function(where) {
+            window.location.assign(where)
+        }
+    })
+});
+
 async function typing() {
     if (i < text.length) {
         document.getElementById("wia").innerHTML += text.charAt(i);
